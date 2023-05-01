28b555e4ef6377e7924a4cbe86a1d8586ba2de86
AndreaMinato <andrea.minato@hotmail.it>
2023-05-01 18:41:43 +0200

    use search input

--- pages/bookings/index.vue
+++ pages/bookings/index.vue
@@ -14,7 +14,7 @@
         <div class="mt-6">
             <h2 class="text-base font-semibold leading-6 text-gray-900">Search</h2>
             <form class="flex gap-2" @submit.prevent="load(true)">
-                <input v-model="search" type="text" name="search" id="search" autocomplete="off"
+                <input v-model="search" type="search" name="search" id="search" autocomplete="off"
                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
 
                 <Button :disabled="loading" type="submit" class="ml-auto">Search
--- pages/travels/index.vue
+++ pages/travels/index.vue
@@ -15,7 +15,7 @@
         <div class="mt-6">
             <h2 class="text-base font-semibold leading-6 text-gray-900">Search</h2>
             <form class="flex gap-2" @submit.prevent="load()">
-                <input v-model="search" type="text" name="search" id="search" autocomplete="off"
+                <input v-model="search" type="search" name="search" id="search" autocomplete="off"
                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
 
                 <div class="mt-2 sm:col-span-2 sm:mt-0 flex-col md:flex-row flex gap-2">
