f599dfed8fa85799b27317f97d9a882d53c612f4
AndreaMinato <andrea.minato@hotmail.it>
2023-05-01 18:45:38 +0200

    remove required text area from payment

--- components/BookingSelectPaymentType.vue
+++ components/BookingSelectPaymentType.vue
@@ -115,7 +115,7 @@
         <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
             <label for="notes" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Notes</label>
             <div class="mt-2 sm:col-span-2 sm:mt-0">
-                <textarea required v-model="notesInfo" id="notes" name="notes" rows="3"
+                <textarea v-model="notesInfo" id="notes" name="notes" rows="3"
                     class="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                 <p class="mt-3 text-sm leading-6 text-gray-600">Tell us everything we need to know.</p>
             </div>
